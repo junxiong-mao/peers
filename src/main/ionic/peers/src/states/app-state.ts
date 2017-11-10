c8a9497f0add1a4865f8c81284b5f42e4a962cec
Sander Valstar <sander@email.com>
2017-11-09 17:59:47 -0800

    adds a loading spinner

---
+++ src/main/ionic/peers/src/states/app-state.ts
@@ -0,0 +1,31 @@
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class AppState {
+  private isLoadingSubject: BehaviorSubject<boolean>;
+  public readonly isLoading: Observable<boolean>;
+
+  private loadingCalls = 0;
+
+  constructor() {
+    this.isLoadingSubject = new BehaviorSubject(false);
+    this.isLoading = this.isLoadingSubject.asObservable();
+  }
+
+  public setIsLoading(isLoading: boolean) {
+    if (isLoading == false) {
+      this.loadingCalls--;
+      if (this.loadingCalls == 0) {
+        this.isLoadingSubject.next(false);
+      }
+    } else {
+      this.loadingCalls++;
+      if (this.loadingCalls == 1) {
+        this.isLoadingSubject.next(true);
+      }
+    }
+  }
+
+}
--- src/main/ionic/peers/src/states/cards-state.ts
+++ src/main/ionic/peers/src/states/cards-state.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { CardsService } from "../services/cards-service";
 import { Injectable } from "@angular/core";
 import { Card } from "../models/card";
+import { AppState } from "./app-state";
 
 @Injectable()
 export class CardsState {
@@ -15,7 +16,7 @@ export class CardsState {
 
   //todo: add isFlipped card state
 
-  constructor(private cardsService: CardsService) {
+  constructor(private cardsService: CardsService, private appState: AppState) {
     this.currentCardSubject = new BehaviorSubject(new Card(null, null, null, [], null));
     this.currentCard = this.currentCardSubject.asObservable();
     this.isMatchSubject = new BehaviorSubject(false);
@@ -23,8 +24,10 @@ export class CardsState {
   }
 
   public initialize() {
+    this.appState.setIsLoading(true);
     // this.cardsService.getCards().subscribe(cards => {
     this.cardsService.getCards().then(response => {
+      this.appState.setIsLoading(false);
       this.cards = response.data;
       this.cardIndex = 0;
       this.currentCardSubject.next(this.cards[this.cardIndex]);
@@ -43,8 +46,12 @@ export class CardsState {
   }
 
   public likeCurrenCard() {
+    this.appState.setIsLoading(true);
     this.cardsService.postDecision(this.currentCardSubject.getValue().id, 'REJECT').subscribe(
-      isMatch => this.isMatchSubject.next(isMatch)
+      isMatch => {
+        this.appState.setIsLoading(false);
+        this.isMatchSubject.next(isMatch)
+      }
     );
   }
 
