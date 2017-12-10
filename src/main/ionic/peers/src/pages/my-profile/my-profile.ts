62170b19bfb2fcbe4d5a2a5b12a383cb6d87811a
junxiong.mao <junxiong.mao@gmail.com>
2017-12-10 02:36:10 -0800

    Remove unused codes

--- src/main/ionic/peers/src/pages/my-profile/my-profile.ts
+++ src/main/ionic/peers/src/pages/my-profile/my-profile.ts
@@ -26,11 +26,6 @@ export class MyProfile implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    /*this.appState.setIsLoading(true);
-    this.userService.getUser().then(response => {
-      this.appState.setIsLoading(false);
-      this.user = response.data;
-    })*/
     this.userSubscription = this.userState.currentUser.subscribe(user => {
       this.user = user;
     });
@@ -41,13 +36,6 @@ export class MyProfile implements OnInit, OnDestroy {
     data.myProfile = this;
     let profileModal = this.modalCtrl.create(EditProfileModal, data);
     profileModal.present();
-  }
-
-  updateView(userInfo) {
-    userInfo.id = this.user.id;
-    userInfo.email = this.user.email;
-    userInfo.photoUrl = this.user.photoUrl;
-    this.user = userInfo;
   }
 
 }
